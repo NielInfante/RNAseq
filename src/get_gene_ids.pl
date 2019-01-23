acc15b0eae2851941755ef623b09752336e39198
Niel Infante <aminfante@hsc.wvu.edu>
2019-01-23 15:52:52 -0500

    Remove old code

--- src/get_gene_ids.pl
+++ src/get_gene_ids.pl
@@ -13,47 +13,16 @@ thirdTry();
 close inFile;
 
 
-sub firstTry{
-    while (my $line = <inFile>){
-	next unless ($line =~ /^\>/);     # Don't want sequence lines
-	
-	my @a = split(/\s+/,$line);
-	$a[0] =~ s/^\>//;
-	
-	if ($a[3] =~ /gene:(\w+)/){
-	    print "$a[0]\t$1\n";
-	} else {
-	    die "Something wrong with:\n$line\n\n";
-	}
-    }
-}
-
-
-sub secondTry{
-    while (my $line = <inFile>){
-	next unless ($line =~ /^\>([\w-]*)\s+/);     # Don't want sequence lines
-
-	my $transcript = $1;
-	
-	if ($line =~ /gene:(\w+)/){
-	    print "$transcript\t$1\n";
-	} else {
-	    die "Something wrong with:\n$line\n\n";
-	}
-    }
-}
-
 sub thirdTry{
-    while (my $line = <inFile>){
-	next unless ($line =~ /^\>([\w-.]*)\s+/);     # Don't want sequence lines
+	while (my $line = <inFile>){
+		next unless ($line =~ /^\>([\w-.]*)\s+/);     # Don't want sequence lines
 
-	my $transcript = $1;
-	
-	if ($line =~ /gene:(\w+)/){
+		my $transcript = $1;
+
+		if ($line =~ /gene:(\w+)/){
 	    print "$transcript\t$1\n";
-	} else {
+		} else {
 	    die "Something wrong with:\n$line\n\n";
 	}
     }
 }
-
